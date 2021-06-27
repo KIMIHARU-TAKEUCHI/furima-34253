01579984ef7a5cc9de04786eba0caed0aee07919
KIMIHARU-TAKEUCHI <takeuti713@gmail.com>
2021-06-27 10:23:52 +0900

    with_options修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-  validates :first_name, presence: true
-  validates :family_name, presence: true
+  validates :first_name
+  validates :family_name
   end
   with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角文字を使用してください' } do
-  validates :first_name_kana, presence: true,
-  validates :family_name_kana, presence: true,
+  validates :first_name_kana
+  validates :family_name_kana
   end
+
   validates :birth_data, presence: true
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
