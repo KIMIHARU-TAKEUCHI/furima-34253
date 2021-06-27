2e0a8123abb21ee2896865c4791c65da8ef912e3
KIMIHARU-TAKEUCHI <takeuti713@gmail.com>
2021-06-27 12:46:32 +0900

    itemsテーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,19 @@
 class ItemsController < ApplicationController
   def index
-    # @items = Items.includes(:user)
+    @items = Items.includes(:user)
+  end
+
+  def new
+    @tweet = Items.new
+  end
+
+  def create
+    Items.create(items_params)
+  end
+
+  private
+  def items_params
+    params.requere(:items).permit(:name, :image, :price, :item_info, :prefectural_id, :item_condition_id, :preparation_day_id, :postage_payer_id, :category_id, :user)
   end
 
 end
---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  def change
+    create_table :items do |t|
+      t.string  :name, null: false
+      t.integer :price, null: false
+      t.text :item_info, null: false
+      t.integer :prefectural_id, null: false
+      t.integer :item_condition_id, null: false
+      t.integer :preparation_day_id, null: false
+      t.integer :postage_payer_id, null: false
+      t.integer :category_id, null: false
+      t.references :user, null: false, foreign_key: true
+    end
+  end
+
+  belongs_to :user
+  
+end
